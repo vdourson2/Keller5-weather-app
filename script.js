6de734b95ddcea86b36468d8c1a90ce030baf412
Virginie Dourson <virginiedourson@gmail.com>
2022-12-22 11:05:36 +0100

    Affichage des données par jour sans style

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //Formulaire d'encodage de la ville
 
 
-//Récupération des données météo pour la ville choisie
-
+//Récupération des données météo de l'API OpenWeather
 // async function getWeatherOnClick () {
 //     try {
 //     console.log("???");
@@ -16,6 +15,7 @@
 //     }
 // }
 
+//Récupération des données de localStorage
 function getWeatherOnClick () {
     let dataWeather = JSON.parse(localStorage.getItem("data"));
     console.log("Récupéré du localStorage: ");
@@ -23,22 +23,49 @@ function getWeatherOnClick () {
     return dataWeather
     }
 
-
-
-//Affichage des données météo récupérées
-//let now = new Date();
+//Affichage des données météo récupérées, jour après jour, toutes les 3 heures
+function displayweatherByHour (){
+    const daysContainer = document.querySelector(".previsions");
+    //initialisation de la variable day qui permettra de séparer les différents jours
+    let previsionDate = new Date(data.list[0].dt*1000);
+    previsionDate.setHours(previsionDate.getUTCHours() + (data.city.timezone/3600))//heure UTC + décalage de timezone
+    let day  = (previsionDate.getDate() - 1);
+    //Création des zones HTML pour chaque prévision de la liste
+    for (let li of data.list){
+        let currentPrevisionDate = new Date(li.dt*1000);
+        currentPrevisionDate.setHours(currentPrevisionDate.getUTCHours() + (data.city.timezone/3600))//heure UTC + décalage de timezone
+        let currentDay  = currentPrevisionDate.getDate();
+        let currentHour = currentPrevisionDate.getHours();
+        //Si on passe au jour suivant, créer une nouvelle div pour le jour courant
+        if (currentDay>day){
+            day += 1;
+            const byDay = `
+                <div class="previsions__day">
+                    <h4 class="previsions__weekday">${currentPrevisionDate.toLocaleString("fr-FR",{weekday:"long"})}</h4>
+                    <div class="previsions__divHours"></div>
+                </div>
+            `;
+            daysContainer.innerHTML += byDay;
+        }
+        //Créer un zone pour la météo à l'heure donnée, et la mettre dans la div du dernier jour courant
+        const byHours = `
+            <div class="previsions__generalParHeure">
+                <p class="previsions__heure">${currentHour}</p>
+                <img class="previsions__icone" src=" http://openweathermap.org/img/wn/${li.weather[0].icon}@2x.png" alt="icone de météo"/>
+                <div class="previsions__humidité">
+                    <div class="previsions__pourcentage"></div>
+                    <div class="previsions__hauteur"></div>
+                </div>
+                <p class="previsions__t"></p>
+                <p class="previsions__descr"></p>
+            </div>
+        `;
+        daysContainer.lastElementChild.querySelector(".previsions__divHours").innerHTML += byHours;
+    }
+}        
 let data = getWeatherOnClick();
-
-console.log("dt");
-console.log(data.list[0].dt);
-let previsionHour = new Date(data.list[0].dt*1000);
-//On prend comme heure locale du lieu demandé : l'heure UTC de la prévision + le décalage
-previsionHour.setHours(previsionHour.getUTCHours() + (data.city.timezone/3600))//heure UTC + décalage de timezone
-console.log(data.city.timezone/3600);//1h
-console.log(previsionHour.getHours()); //Heure locale
-console.log(previsionHour.getDate());//Date locale 
-
-
-
+displayweatherByHour();
+        
+        
 
 
